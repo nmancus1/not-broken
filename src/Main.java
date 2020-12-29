b5ab374b288d9c4ca07e9bb57559cc3f136f3010
Nick Mancuso <nmancus1@dtcc.edu>
2020-12-29 09:10:43 -0500

    added code

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,26 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        HashMap<String, Integer> map = new HashMap<>(); // trailing comment here
+        HashMap<String,Integer> map = new HashMap<>();//trailing comment here
         int ASDFASDF = 0;
-        switch (ASDFASDF) {
+        int x = 23423;
+        String value5 = """
+            \u03bc\s""";
+
+        switch(ASDFASDF){
+            default:
+                System.out.println("default!");
             case 1:
                 System.out.println(ASDFASDF);
         }
-        for (int i = 0; i < 10; i++)
-            for (int j = 0; j < 10; j++)
-                for (int k = 0; k < 10; k++)
-                    for (int l = 0; l < 10; l++)
+        for (int i = 0; i < 10; i++){
+            for (int j = 0;j < 10;j++){
+                for (int k = 0; k < 10; k++) {
+                    for (int l = 0; l < 10; l++) {
                         System.out.println("Too many nested loops!");
+                    }
+                }
+            }
+        }
     }
 }
